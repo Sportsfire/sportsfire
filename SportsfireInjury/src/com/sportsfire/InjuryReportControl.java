6e814c0ac24712331b88411677fe3b0731ab087d
elliott.omosheye <e>
2012-12-17 14:40:06 +0000

    fixed merge issues
    Don't push to master anymore we need to discuss how we are going to do
    things going foward

--- SportsfireInjury/src/com/sportsfire/InjuryReportControl.java
+++ SportsfireInjury/src/com/sportsfire/InjuryReportControl.java
@@ -1,6 +1,5 @@
 package com.sportsfire;
 
-<<<<<<< HEAD
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -14,220 +13,202 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-=======
 import com.sportsfire.db.InjuryTable;
 import com.sportsfire.sportsfireinjury.R;
 
->>>>>>> fab1d6cf3062df8a0b9b0084c368fb0db33a92aa
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.util.Log;
 
-public class InjuryReportControl{
-	private HashMap<String,String> changedData = new HashMap<String,String>();
-	private HashMap<String,String> oldData = new HashMap<String,String>();
+public class InjuryReportControl {
+	private HashMap<String, String> changedData = new HashMap<String, String>();
+	private HashMap<String, String> oldData = new HashMap<String, String>();
 	private Context context;
 	private String playerID;
 	private String injuryID;
 	boolean newReport;
-    
-    // creates a new injury report for Player p
-    public InjuryReportControl(Player p,Context c){
-        Log.e("### Creating new Injury Report", "...");
-        newReport = true;
-        playerID = p.getID();
-        context = c;
-    }
-    
-    // loads an injury report with ID == formID
-    public InjuryReportControl(InjuryReportID formID,Context c){
-        Log.e("###  Loading existing injury report", "...");
-        newReport = false;
-        context = c;
-        playerID = ""; // here we need to set this from DB later
-        
-        injuryID = formID.getID();
-        
-     // open db for read / write
-    	DBHelper dbHelp = new DBHelper(context);
-        SQLiteDatabase db = dbHelp.getReadableDatabase();
-        
-        String selectSquadData = "SELECT  * FROM " + InjuryTable.TABLE_NAME + " WHERE "+InjuryTable.KEY_INJURY_ID + " = "+ injuryID+";";
-        Cursor cursor = db.rawQuery(selectSquadData, null);
-        if (cursor.moveToFirst()) {
-            do {
-            	for(int i=0; i<cursor.getColumnCount();i++){
-            		oldData.put(cursor.getColumnName(i), cursor.getString(i));
-            	}
-            	
-            } while (cursor.moveToNext());
-        }
-        dbHelp.close();
-    }
-    
-    private void createNewReport(SQLiteDatabase db){
-    
-        
-        // Open for Read-Only
-        //db = dBHelper.getReadableDatabase();
 
-    	
-    	
-        ContentValues values = new ContentValues();
-        // No need to include squad id, is automatically added
-        values.put(InjuryTable.KEY_PLAYER_ID, playerID);
-        Log.e("### Adding a new injury", "...");
-        long rowID = db.insert(InjuryTable.TABLE_NAME, null, values);
-        
-        /*String getInjuryID = "SELECT * FROM " + InjuryTable.TABLE_NAME + " WHERE "+InjuryTable.KEY_PLAYER_ID+" = '"+playerID+"';";
-        Cursor cursor = db.rawQuery(getInjuryID, null);
-        if (cursor.moveToFirst()) {
-             injuryID = cursor.getString(0);
-        }*/
-        injuryID = String.valueOf(rowID);
-        newReport = false;
-    }
-    
-    public void saveForm(){
-    	// open db for read / write
-    	DBHelper dbHelp = new DBHelper(context);
-        SQLiteDatabase db = dbHelp.getWritableDatabase();
-        
-    	if(newReport == true){
-    		createNewReport(db);
-    	}
-    	
-    	// process changes
-    	ContentValues values = new ContentValues();
-    	Iterator it = changedData.entrySet().iterator();
-    	while(it.hasNext()){
-    		Map.Entry<String, String> pairs = (Map.Entry<String, String>)it.next();
-    		
-    			values.put(pairs.getKey(),pairs.getValue());
-    		
-    	}
-    	db.update(InjuryTable.TABLE_NAME, values, InjuryTable.KEY_INJURY_ID + " = '"+injuryID+"'",null);    	
-    }
-    
-    private String getStandardValue(String field){
-    	if(field.equals(InjuryTable.KEY_DATE_OF_INJURY) || 
-    			field.equals(InjuryTable.KEY_DATE_OF_RETURN) || 
-    			field.equals(InjuryTable.KEY_PREVIOUS_DATE) || 
-    			field.equals(InjuryTable.KEY_DIAGNOSIS) || 
-    			field.equals(InjuryTable.KEY_INJURED_BODY_PART)){
-    		return "";
-    	}
-    	return "0";
-    }
-	private String idToField(int id){
-		switch(id){
-		case R.id.ir1a:
-			return InjuryTable.KEY_DATE_OF_INJURY;
-		case R.id.ir1b:
-			return InjuryTable.KEY_DATE_OF_RETURN;
-		case R.id.ir2a1:
-			return InjuryTable.KEY_BODYPART_HEAD;
-		case R.id.ir2a2:
-			return InjuryTable.KEY_BODYPART_SHOULDER;
-		case R.id.ir2a3:
-			return InjuryTable.KEY_BODYPART_HIP;
-		case R.id.ir2a4:
-			return InjuryTable.KEY_BODYPART_NECK;
-		case R.id.ir2a5:
-			return InjuryTable.KEY_BODYPART_UPPERARM;
-		case R.id.ir2a6:
-			return InjuryTable.KEY_BODYPART_THIGH;
-		case R.id.ir2a7:
-			return InjuryTable.KEY_BODYPART_STERNUM;
-		case R.id.ir2a8:
-			return InjuryTable.KEY_BODYPART_ELBOW;
-		case R.id.ir2a9:
-			return InjuryTable.KEY_BODYPART_KNEE;
-		case R.id.ir2a10:
-			return InjuryTable.KEY_BODYPART_ABDOMEN;
-		case R.id.ir2a11:
-			return InjuryTable.KEY_BODYPART_FOREARM;
-		case R.id.ir2a12:
-			return InjuryTable.KEY_BODYPART_LOWERLEG;
-		case R.id.ir2a13:
-			return InjuryTable.KEY_BODYPART_LOWBACK;
-		case R.id.ir2a14:
-			return InjuryTable.KEY_BODYPART_WRIST;
-		case R.id.ir2a15:
-			return InjuryTable.KEY_BODYPART_ANKLE;
-		case R.id.ir2a16:
-			return InjuryTable.KEY_BODYPART_HAND;
-		case R.id.ir2a17:
-			return InjuryTable.KEY_BODYPART_FOOT;
-		case R.id.ir31:
-			return InjuryTable.KEY_TYPE_CONCUSSION;
-		case R.id.ir32:
-			return InjuryTable.KEY_TYPE_LESION;
-		case R.id.ir33:
-			return InjuryTable.KEY_TYPE_HAEMATOMA;
-		case R.id.ir34:
-			return InjuryTable.KEY_TYPE_FRACTURE;
-		case R.id.ir35:
-			return InjuryTable.KEY_TYPE_MUSCLE;
-		case R.id.ir36:
-			return InjuryTable.KEY_TYPE_ABRASION;
-		case R.id.ir37:
-			return InjuryTable.KEY_TYPE_OTHERBONE;
-		case R.id.ir38:
-			return InjuryTable.KEY_TYPE_LACERATION;
-		case R.id.ir39:
-			return InjuryTable.KEY_TYPE_DISLOCATION;
-		case R.id.ir310:
-			return InjuryTable.KEY_TYPE_TENDON;
-		case R.id.ir311:
-			return InjuryTable.KEY_TYPE_NERVE;
-		case R.id.ir312:
-			return InjuryTable.KEY_TYPE_SPRAIN;
-		case R.id.ir313:
-			return InjuryTable.KEY_TYPE_DENTAL;
-		default:
-			return "";
-		}		
+	// creates a new injury report for Player p
+	public InjuryReportControl(Player p, Context c) {
+		Log.e("### Creating new Injury Report", "...");
+		newReport = true;
+		playerID = p.getID();
+		context = c;
 	}
-    // maybe replace field name String with enum?
-    public String getValue(String field){
-        String result = changedData.get(field);
-        
-        if(result != null)
-        	return result;
-       
-        if(newReport){
-        	return getStandardValue(field);
-        }
-        
-        result = oldData.get(field);
-       
-        if(result == null){
-        	result = "";
-        	Log.e("getValueError",field);
-        }
-        
-        return result;    
-    }
-    
-<<<<<<< HEAD
-    public void setValue(String field,String value){
-    	changedData.put(field, value);
-=======
-   /* public void setValue(String field,InjuryReportValue value){
-    	if(((InjuryReportValueBoolean)value).value == true)
-    		Log.e("checked", field);
-    	else
-    		Log.e("unchecked", field);
-    }*/
-    public void setValue(int id,InjuryReportValue value){
-    	String field = idToField(id);
-    	if(((InjuryReportValueBoolean)value).value == true)
-    		Log.e("checked", field);
-    	else
-    		Log.e("unchecked", field);
->>>>>>> fab1d6cf3062df8a0b9b0084c368fb0db33a92aa
-    }
-    
-    
+
+	// loads an injury report with ID == formID
+	public InjuryReportControl(InjuryReportID formID, Context c) {
+		Log.e("###  Loading existing injury report", "...");
+		newReport = false;
+		context = c;
+		playerID = ""; // here we need to set this from DB later
+
+		injuryID = formID.getID();
+
+		// open db for read / write
+		DBHelper dbHelp = new DBHelper(context);
+		SQLiteDatabase db = dbHelp.getReadableDatabase();
+
+		String selectSquadData = "SELECT  * FROM " + InjuryTable.TABLE_NAME + " WHERE "
+				+ InjuryTable.KEY_INJURY_ID + " = " + injuryID + ";";
+		Cursor cursor = db.rawQuery(selectSquadData, null);
+		if (cursor.moveToFirst()) {
+			do {
+				for (int i = 0; i < cursor.getColumnCount(); i++) {
+					oldData.put(cursor.getColumnName(i), cursor.getString(i));
+				}
+
+			} while (cursor.moveToNext());
+		}
+		dbHelp.close();
+	}
+
+	private void createNewReport(SQLiteDatabase db) {
+
+		// Open for Read-Only
+		// db = dBHelper.getReadableDatabase();
+
+		ContentValues values = new ContentValues();
+		// No need to include squad id, is automatically added
+		values.put(InjuryTable.KEY_PLAYER_ID, playerID);
+		Log.e("### Adding a new injury", "...");
+		long rowID = db.insert(InjuryTable.TABLE_NAME, null, values);
+
+		/*
+		 * String getInjuryID = "SELECT * FROM " + InjuryTable.TABLE_NAME +
+		 * " WHERE "+InjuryTable.KEY_PLAYER_ID+" = '"+playerID+"';"; Cursor
+		 * cursor = db.rawQuery(getInjuryID, null); if (cursor.moveToFirst()) {
+		 * injuryID = cursor.getString(0); }
+		 */
+		injuryID = String.valueOf(rowID);
+		newReport = false;
+	}
+
+	public void saveForm() {
+		// open db for read / write
+		DBHelper dbHelp = new DBHelper(context);
+		SQLiteDatabase db = dbHelp.getWritableDatabase();
+
+		if (newReport == true) {
+			createNewReport(db);
+		}
+
+		// process changes
+		ContentValues values = new ContentValues();
+		Iterator it = changedData.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry<String, String> pairs = (Map.Entry<String, String>) it.next();
+
+			values.put(pairs.getKey(), pairs.getValue());
+
+		}
+		db.update(InjuryTable.TABLE_NAME, values, InjuryTable.KEY_INJURY_ID + " = '" + injuryID
+				+ "'", null);
+	}
+
+	private String getStandardValue(String field) {
+		if (field.equals(InjuryTable.KEY_DATE_OF_INJURY)
+				|| field.equals(InjuryTable.KEY_DATE_OF_RETURN)
+				|| field.equals(InjuryTable.KEY_PREVIOUS_DATE)
+				|| field.equals(InjuryTable.KEY_DIAGNOSIS)
+				|| field.equals(InjuryTable.KEY_INJURED_BODY_PART)) {
+			return "";
+		}
+		return "0";
+	}
+	private String idToField(int id) {
+		switch (id) {
+			case R.id.ir1a :
+				return InjuryTable.KEY_DATE_OF_INJURY;
+			case R.id.ir1b :
+				return InjuryTable.KEY_DATE_OF_RETURN;
+			case R.id.ir2a1 :
+				return InjuryTable.KEY_BODYPART_HEAD;
+			case R.id.ir2a2 :
+				return InjuryTable.KEY_BODYPART_SHOULDER;
+			case R.id.ir2a3 :
+				return InjuryTable.KEY_BODYPART_HIP;
+			case R.id.ir2a4 :
+				return InjuryTable.KEY_BODYPART_NECK;
+			case R.id.ir2a5 :
+				return InjuryTable.KEY_BODYPART_UPPERARM;
+			case R.id.ir2a6 :
+				return InjuryTable.KEY_BODYPART_THIGH;
+			case R.id.ir2a7 :
+				return InjuryTable.KEY_BODYPART_STERNUM;
+			case R.id.ir2a8 :
+				return InjuryTable.KEY_BODYPART_ELBOW;
+			case R.id.ir2a9 :
+				return InjuryTable.KEY_BODYPART_KNEE;
+			case R.id.ir2a10 :
+				return InjuryTable.KEY_BODYPART_ABDOMEN;
+			case R.id.ir2a11 :
+				return InjuryTable.KEY_BODYPART_FOREARM;
+			case R.id.ir2a12 :
+				return InjuryTable.KEY_BODYPART_LOWERLEG;
+			case R.id.ir2a13 :
+				return InjuryTable.KEY_BODYPART_LOWBACK;
+			case R.id.ir2a14 :
+				return InjuryTable.KEY_BODYPART_WRIST;
+			case R.id.ir2a15 :
+				return InjuryTable.KEY_BODYPART_ANKLE;
+			case R.id.ir2a16 :
+				return InjuryTable.KEY_BODYPART_HAND;
+			case R.id.ir2a17 :
+				return InjuryTable.KEY_BODYPART_FOOT;
+			case R.id.ir31 :
+				return InjuryTable.KEY_TYPE_CONCUSSION;
+			case R.id.ir32 :
+				return InjuryTable.KEY_TYPE_LESION;
+			case R.id.ir33 :
+				return InjuryTable.KEY_TYPE_HAEMATOMA;
+			case R.id.ir34 :
+				return InjuryTable.KEY_TYPE_FRACTURE;
+			case R.id.ir35 :
+				return InjuryTable.KEY_TYPE_MUSCLE;
+			case R.id.ir36 :
+				return InjuryTable.KEY_TYPE_ABRASION;
+			case R.id.ir37 :
+				return InjuryTable.KEY_TYPE_OTHERBONE;
+			case R.id.ir38 :
+				return InjuryTable.KEY_TYPE_LACERATION;
+			case R.id.ir39 :
+				return InjuryTable.KEY_TYPE_DISLOCATION;
+			case R.id.ir310 :
+				return InjuryTable.KEY_TYPE_TENDON;
+			case R.id.ir311 :
+				return InjuryTable.KEY_TYPE_NERVE;
+			case R.id.ir312 :
+				return InjuryTable.KEY_TYPE_SPRAIN;
+			case R.id.ir313 :
+				return InjuryTable.KEY_TYPE_DENTAL;
+			default :
+				return "";
+		}
+	}
+	// maybe replace field name String with enum?
+	public String getValue(String field) {
+		String result = changedData.get(field);
+
+		if (result != null)
+			return result;
+
+		if (newReport) {
+			return getStandardValue(field);
+		}
+
+		result = oldData.get(field);
+
+		if (result == null) {
+			result = "";
+			Log.e("getValueError", field);
+		}
+
+		return result;
+	}
+
+	public void setValue(String field, String value) {
+		changedData.put(field, value);
+	}
+
 }
--- SportsfireInjury/src/com/sportsfire/sportsfireinjury/InjuryForm.java
+++ SportsfireInjury/src/com/sportsfire/sportsfireinjury/InjuryForm.java
@@ -14,186 +14,176 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
-
 public class InjuryForm extends Activity {
 	public static final String ARG_ITEM_INJURY = "argumentInjuryID";
 	public static final String ARG_ITEM_PLAYER = "argumentPlayer";
-	
-	
+
 	private InjuryReportControl reportControl;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.player_injury_form);
 		Player p = getIntent().getParcelableExtra(ARG_ITEM_PLAYER);
-		if(p != null){
-			reportControl = new InjuryReportControl(p,this);
-		}
-		else{
+		if (p != null) {
+			reportControl = new InjuryReportControl(p, this);
+		} else {
 			InjuryReportID id = getIntent().getParcelableExtra(ARG_ITEM_INJURY);
-			reportControl = new InjuryReportControl(id,this);
+			reportControl = new InjuryReportControl(id, this);
 		}
-		//if (details != null) {
-		//	((TextView) findViewById(R.id.EditText01)).setText("10/10/10");
-		//}
-	//	((TextView)findViewById(R.id.ir314)).setSelected(false);
-		//((TextView)findViewById(R.id.ir4)).setSelected(false);
-		
-		((TextView)findViewById(R.id.ir1a)).addTextChangedListener(new TextWatcher() { 
+		// if (details != null) {
+		// ((TextView) findViewById(R.id.EditText01)).setText("10/10/10");
+		// }
+		// ((TextView)findViewById(R.id.ir314)).setSelected(false);
+		// ((TextView)findViewById(R.id.ir4)).setSelected(false);
 
-            public void  afterTextChanged (Editable s){ 
-                   // Log.d("seachScreen", "afterTextChanged");
-            	reportControl.setValue(idToField(R.id.ir1a),s.toString());
-            } 
-            public void  beforeTextChanged  (CharSequence s, int start, int 
-count, int after){ 
-                   // Log.d("seachScreen", "beforeTextChanged"); 
-            } 
-            public void  onTextChanged  (CharSequence s, int start, int before, 
-int count) { 
-                   // Log.d("seachScreen", "onTextChanged"); 
-            }
-		});
-		
-		((TextView)findViewById(R.id.ir1b)).addTextChangedListener(new TextWatcher() { 
+		((TextView) findViewById(R.id.ir1a)).addTextChangedListener(new TextWatcher() {
 
-            public void  afterTextChanged (Editable s){ 
-                   // Log.d("seachScreen", "afterTextChanged");
-            	reportControl.setValue(idToField(R.id.ir1b),s.toString());
-            } 
-            public void  beforeTextChanged  (CharSequence s, int start, int 
-count, int after){ 
-                   // Log.d("seachScreen", "beforeTextChanged"); 
-            } 
-            public void  onTextChanged  (CharSequence s, int start, int before, 
-int count) { 
-                   // Log.d("seachScreen", "onTextChanged"); 
-            }
+			public void afterTextChanged(Editable s) {
+				// Log.d("seachScreen", "afterTextChanged");
+				reportControl.setValue(idToField(R.id.ir1a), s.toString());
+			}
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+				// Log.d("seachScreen", "beforeTextChanged");
+			}
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				// Log.d("seachScreen", "onTextChanged");
+			}
 		});
-		
-		((TextView)findViewById(R.id.ir4)).addTextChangedListener(new TextWatcher() { 
 
-            public void  afterTextChanged (Editable s){ 
-                   // Log.d("seachScreen", "afterTextChanged");
-            	reportControl.setValue(idToField(R.id.ir4),s.toString());
-            } 
-            public void  beforeTextChanged  (CharSequence s, int start, int 
-count, int after){ 
-                   // Log.d("seachScreen", "beforeTextChanged"); 
-            } 
-            public void  onTextChanged  (CharSequence s, int start, int before, 
-int count) { 
-                   // Log.d("seachScreen", "onTextChanged"); 
-            }
+		((TextView) findViewById(R.id.ir1b)).addTextChangedListener(new TextWatcher() {
+
+			public void afterTextChanged(Editable s) {
+				// Log.d("seachScreen", "afterTextChanged");
+				reportControl.setValue(idToField(R.id.ir1b), s.toString());
+			}
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+				// Log.d("seachScreen", "beforeTextChanged");
+			}
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				// Log.d("seachScreen", "onTextChanged");
+			}
 		});
-		
+
+		((TextView) findViewById(R.id.ir4)).addTextChangedListener(new TextWatcher() {
+
+			public void afterTextChanged(Editable s) {
+				// Log.d("seachScreen", "afterTextChanged");
+				reportControl.setValue(idToField(R.id.ir4), s.toString());
+			}
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+				// Log.d("seachScreen", "beforeTextChanged");
+			}
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				// Log.d("seachScreen", "onTextChanged");
+			}
+		});
+
 		// Setup everything
-		((TextView)findViewById(R.id.ir1a)).setText(reportControl.getValue(InjuryTable.KEY_DATE_OF_INJURY));
-		((TextView)findViewById(R.id.ir1b)).setText(reportControl.getValue(InjuryTable.KEY_DATE_OF_RETURN));
-		((TextView)findViewById(R.id.ir4)).setText(reportControl.getValue(InjuryTable.KEY_DIAGNOSIS));
-		textBoxSetup(R.id.ir2a1,InjuryTable.KEY_BODYPART_HEAD);
-		textBoxSetup(R.id.ir2a2,InjuryTable.KEY_BODYPART_SHOULDER);
-		textBoxSetup(R.id.ir2a3,InjuryTable.KEY_BODYPART_HIP);
-		textBoxSetup(R.id.ir2a4,InjuryTable.KEY_BODYPART_NECK);
-		textBoxSetup(R.id.ir2a5,InjuryTable.KEY_BODYPART_UPPERARM);
-		textBoxSetup(R.id.ir2a6,InjuryTable.KEY_BODYPART_THIGH);
-		textBoxSetup(R.id.ir2a7,InjuryTable.KEY_BODYPART_STERNUM);
-		textBoxSetup(R.id.ir2a8,InjuryTable.KEY_BODYPART_ELBOW);
-		textBoxSetup(R.id.ir2a9,InjuryTable.KEY_BODYPART_KNEE);
-		textBoxSetup(R.id.ir2a10,InjuryTable.KEY_BODYPART_ABDOMEN);
-		textBoxSetup(R.id.ir2a11,InjuryTable.KEY_BODYPART_FOREARM);
-		textBoxSetup(R.id.ir2a12,InjuryTable.KEY_BODYPART_LOWERLEG);
-		textBoxSetup(R.id.ir2a13,InjuryTable.KEY_BODYPART_LOWBACK);
-		textBoxSetup(R.id.ir2a14,InjuryTable.KEY_BODYPART_WRIST);
-		textBoxSetup(R.id.ir2a15,InjuryTable.KEY_BODYPART_ANKLE);
-		textBoxSetup(R.id.ir2a16,InjuryTable.KEY_BODYPART_HAND);
-		textBoxSetup(R.id.ir2a17,InjuryTable.KEY_BODYPART_FOOT);		
+		((TextView) findViewById(R.id.ir1a)).setText(reportControl
+				.getValue(InjuryTable.KEY_DATE_OF_INJURY));
+		((TextView) findViewById(R.id.ir1b)).setText(reportControl
+				.getValue(InjuryTable.KEY_DATE_OF_RETURN));
+		((TextView) findViewById(R.id.ir4)).setText(reportControl
+				.getValue(InjuryTable.KEY_DIAGNOSIS));
+		textBoxSetup(R.id.ir2a1, InjuryTable.KEY_BODYPART_HEAD);
+		textBoxSetup(R.id.ir2a2, InjuryTable.KEY_BODYPART_SHOULDER);
+		textBoxSetup(R.id.ir2a3, InjuryTable.KEY_BODYPART_HIP);
+		textBoxSetup(R.id.ir2a4, InjuryTable.KEY_BODYPART_NECK);
+		textBoxSetup(R.id.ir2a5, InjuryTable.KEY_BODYPART_UPPERARM);
+		textBoxSetup(R.id.ir2a6, InjuryTable.KEY_BODYPART_THIGH);
+		textBoxSetup(R.id.ir2a7, InjuryTable.KEY_BODYPART_STERNUM);
+		textBoxSetup(R.id.ir2a8, InjuryTable.KEY_BODYPART_ELBOW);
+		textBoxSetup(R.id.ir2a9, InjuryTable.KEY_BODYPART_KNEE);
+		textBoxSetup(R.id.ir2a10, InjuryTable.KEY_BODYPART_ABDOMEN);
+		textBoxSetup(R.id.ir2a11, InjuryTable.KEY_BODYPART_FOREARM);
+		textBoxSetup(R.id.ir2a12, InjuryTable.KEY_BODYPART_LOWERLEG);
+		textBoxSetup(R.id.ir2a13, InjuryTable.KEY_BODYPART_LOWBACK);
+		textBoxSetup(R.id.ir2a14, InjuryTable.KEY_BODYPART_WRIST);
+		textBoxSetup(R.id.ir2a15, InjuryTable.KEY_BODYPART_ANKLE);
+		textBoxSetup(R.id.ir2a16, InjuryTable.KEY_BODYPART_HAND);
+		textBoxSetup(R.id.ir2a17, InjuryTable.KEY_BODYPART_FOOT);
 	}
-	
-	private void textBoxSetup(int id,String field){
-		if(reportControl.getValue(field).equals("0")){
-			((CheckBox)findViewById(id)).setChecked(false);
+
+	private void textBoxSetup(int id, String field) {
+		if (reportControl.getValue(field).equals("0")) {
+			((CheckBox) findViewById(id)).setChecked(false);
+		} else {
+			((CheckBox) findViewById(id)).setChecked(true);
 		}
-		else{
-			((CheckBox)findViewById(id)).setChecked(true);
+	}
+
+	private String idToField(int id) {
+		switch (id) {
+			case R.id.ir1a :
+				return InjuryTable.KEY_DATE_OF_INJURY;
+			case R.id.ir1b :
+				return InjuryTable.KEY_DATE_OF_RETURN;
+			case R.id.ir2a1 :
+				return InjuryTable.KEY_BODYPART_HEAD;
+			case R.id.ir2a2 :
+				return InjuryTable.KEY_BODYPART_SHOULDER;
+			case R.id.ir2a3 :
+				return InjuryTable.KEY_BODYPART_HIP;
+			case R.id.ir2a4 :
+				return InjuryTable.KEY_BODYPART_NECK;
+			case R.id.ir2a5 :
+				return InjuryTable.KEY_BODYPART_UPPERARM;
+			case R.id.ir2a6 :
+				return InjuryTable.KEY_BODYPART_THIGH;
+			case R.id.ir2a7 :
+				return InjuryTable.KEY_BODYPART_STERNUM;
+			case R.id.ir2a8 :
+				return InjuryTable.KEY_BODYPART_ELBOW;
+			case R.id.ir2a9 :
+				return InjuryTable.KEY_BODYPART_KNEE;
+			case R.id.ir2a10 :
+				return InjuryTable.KEY_BODYPART_ABDOMEN;
+			case R.id.ir2a11 :
+				return InjuryTable.KEY_BODYPART_FOREARM;
+			case R.id.ir2a12 :
+				return InjuryTable.KEY_BODYPART_LOWERLEG;
+			case R.id.ir2a13 :
+				return InjuryTable.KEY_BODYPART_LOWBACK;
+			case R.id.ir2a14 :
+				return InjuryTable.KEY_BODYPART_WRIST;
+			case R.id.ir2a15 :
+				return InjuryTable.KEY_BODYPART_ANKLE;
+			case R.id.ir2a16 :
+				return InjuryTable.KEY_BODYPART_HAND;
+			case R.id.ir2a17 :
+				return InjuryTable.KEY_BODYPART_FOOT;
+			case R.id.ir31 :
+				return InjuryTable.KEY_TYPE_CONCUSSION;
+			case R.id.ir32 :
+				return InjuryTable.KEY_TYPE_LESION;
+			case R.id.ir33 :
+				return InjuryTable.KEY_TYPE_HAEMATOMA;
+			case R.id.ir34 :
+				return InjuryTable.KEY_TYPE_FRACTURE;
+			case R.id.ir35 :
+				return InjuryTable.KEY_TYPE_MUSCLE;
+			case R.id.ir36 :
+				return InjuryTable.KEY_TYPE_ABRASION;
+			case R.id.ir37 :
+				return InjuryTable.KEY_TYPE_OTHERBONE;
+			case R.id.ir38 :
+				return InjuryTable.KEY_TYPE_LACERATION;
+			case R.id.ir39 :
+				return InjuryTable.KEY_TYPE_DISLOCATION;
+			case R.id.ir310 :
+				return InjuryTable.KEY_TYPE_TENDON;
+			case R.id.ir311 :
+				return InjuryTable.KEY_TYPE_NERVE;
+			case R.id.ir312 :
+				return InjuryTable.KEY_TYPE_SPRAIN;
+			case R.id.ir313 :
+				return InjuryTable.KEY_TYPE_DENTAL;
+			case R.id.ir4 :
+				return InjuryTable.KEY_DIAGNOSIS;
+			default :
+				return "";
 		}
 	}
-	
-<<<<<<< HEAD
-	private String idToField(int id){
-		switch(id){
-		case R.id.ir1a:
-			return InjuryTable.KEY_DATE_OF_INJURY;
-		case R.id.ir1b:
-			return InjuryTable.KEY_DATE_OF_RETURN;
-		case R.id.ir2a1:
-			return InjuryTable.KEY_BODYPART_HEAD;
-		case R.id.ir2a2:
-			return InjuryTable.KEY_BODYPART_SHOULDER;
-		case R.id.ir2a3:
-			return InjuryTable.KEY_BODYPART_HIP;
-		case R.id.ir2a4:
-			return InjuryTable.KEY_BODYPART_NECK;
-		case R.id.ir2a5:
-			return InjuryTable.KEY_BODYPART_UPPERARM;
-		case R.id.ir2a6:
-			return InjuryTable.KEY_BODYPART_THIGH;
-		case R.id.ir2a7:
-			return InjuryTable.KEY_BODYPART_STERNUM;
-		case R.id.ir2a8:
-			return InjuryTable.KEY_BODYPART_ELBOW;
-		case R.id.ir2a9:
-			return InjuryTable.KEY_BODYPART_KNEE;
-		case R.id.ir2a10:
-			return InjuryTable.KEY_BODYPART_ABDOMEN;
-		case R.id.ir2a11:
-			return InjuryTable.KEY_BODYPART_FOREARM;
-		case R.id.ir2a12:
-			return InjuryTable.KEY_BODYPART_LOWERLEG;
-		case R.id.ir2a13:
-			return InjuryTable.KEY_BODYPART_LOWBACK;
-		case R.id.ir2a14:
-			return InjuryTable.KEY_BODYPART_WRIST;
-		case R.id.ir2a15:
-			return InjuryTable.KEY_BODYPART_ANKLE;
-		case R.id.ir2a16:
-			return InjuryTable.KEY_BODYPART_HAND;
-		case R.id.ir2a17:
-			return InjuryTable.KEY_BODYPART_FOOT;
-		case R.id.ir31:
-			return InjuryTable.KEY_TYPE_CONCUSSION;
-		case R.id.ir32:
-			return InjuryTable.KEY_TYPE_LESION;
-		case R.id.ir33:
-			return InjuryTable.KEY_TYPE_HAEMATOMA;
-		case R.id.ir34:
-			return InjuryTable.KEY_TYPE_FRACTURE;
-		case R.id.ir35:
-			return InjuryTable.KEY_TYPE_MUSCLE;
-		case R.id.ir36:
-			return InjuryTable.KEY_TYPE_ABRASION;
-		case R.id.ir37:
-			return InjuryTable.KEY_TYPE_OTHERBONE;
-		case R.id.ir38:
-			return InjuryTable.KEY_TYPE_LACERATION;
-		case R.id.ir39:
-			return InjuryTable.KEY_TYPE_DISLOCATION;
-		case R.id.ir310:
-			return InjuryTable.KEY_TYPE_TENDON;
-		case R.id.ir311:
-			return InjuryTable.KEY_TYPE_NERVE;
-		case R.id.ir312:
-			return InjuryTable.KEY_TYPE_SPRAIN;
-		case R.id.ir313:
-			return InjuryTable.KEY_TYPE_DENTAL;
-		case R.id.ir4:
-			return InjuryTable.KEY_DIAGNOSIS;
-		default:
-			return "";
-		}		
-	}
-=======
->>>>>>> fab1d6cf3062df8a0b9b0084c368fb0db33a92aa
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -205,27 +195,18 @@ int count) {
 		DialogFragment newFragment = new DatePickerFragment((TextView) v);
 		newFragment.show(getFragmentManager(), "datePicker");
 	}
-	
-	public void onCheckBoxClick(View v){
-		CheckBox c = (CheckBox)v;
-        if(c.isChecked()){
-<<<<<<< HEAD
-        	reportControl.setValue(idToField(c.getId()), "1");
-        }
-        else{
-        	reportControl.setValue(idToField(c.getId()), "0");
-=======
-        	//reportControl.setValue(idToField(c.getId()), new InjuryReportValueBoolean(true));
-        	reportControl.setValue(c.getId(), new InjuryReportValueBoolean(true));
-        }
-        else{
-        	//reportControl.setValue(idToField(c.getId()), new InjuryReportValueBoolean(false));
-        	reportControl.setValue(c.getId(), new InjuryReportValueBoolean(false));
->>>>>>> fab1d6cf3062df8a0b9b0084c368fb0db33a92aa
-        }
+
+	public void onCheckBoxClick(View v) {
+		CheckBox c = (CheckBox) v;
+		if (c.isChecked()) {
+
+			reportControl.setValue(idToField(c.getId()), "1");
+		} else {
+			reportControl.setValue(idToField(c.getId()), "0");
+		}
 	}
-	
-	public void onSaveForm(View v){
+
+	public void onSaveForm(View v) {
 		reportControl.saveForm();
 	}
 }
