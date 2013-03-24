d563b465a433250ae67153179b1a64ccc45e9950
elliott.omosheye <e>
2013-03-24 15:30:12 +0000

    test update

--- SportsFireScreening/tests/src/com/sportsfire/screening/test/ScreeningDataTest.java
+++ SportsFireScreening/tests/src/com/sportsfire/screening/test/ScreeningDataTest.java
@@ -64,57 +64,81 @@ public class ScreeningDataTest extends ProviderTestCase2<Provider> {
 
 	public void testNewScreeningResultSaving() {
 		String expected = "110294.90";
-		ScreeningData data = new ScreeningData(getContext(), seasonID,"1");
+		ScreeningData data = new ScreeningData(getContext(), seasonID, "1");
 		data.setValue(playerID, "TEST", expected);
 		assertEquals(expected, data.getValue(playerID, "TEST"));
 	}
+
+	public void testChangeSeasonsResultSaving(){
+		String expected = "110294.90";
+		ScreeningData data = new ScreeningData(getContext(), seasonID, "1");
+		data.setValue(playerID, "TEST", expected);
+		assertEquals(expected, data.getValue(playerID, "TEST"));
+		String expected2 = "57856.32";
+		String differentSeason = "11";
+		ScreeningData data2 = new ScreeningData(getContext(), differentSeason, "1");
+		data2.setValue(playerID, "TEST", expected2);
+		assertEquals(expected2, data2.getValue(playerID, "TEST"));
+		assertEquals(expected, data.getValue(playerID, "TEST"));
+		
+	}
+	
 	public void testAvgResult() {
 		String expected = "250.00";
-		ScreeningData data = new ScreeningData(getContext(), seasonID,"1");
+		ScreeningData data = new ScreeningData(getContext(), seasonID, "1");
 		data.setValue(playerID, "TEST", "200");
-		data = new ScreeningData(getContext(), seasonID,"2");
+		data = new ScreeningData(getContext(), seasonID, "2");
 		data.setValue(playerID, "TEST", "300");
-		data = new ScreeningData(getContext(), seasonID,"3");
+		data = new ScreeningData(getContext(), seasonID, "3");
 		data.setValue(playerID, "TEST", "350");
-		data = new ScreeningData(getContext(), seasonID,"4");
+		data = new ScreeningData(getContext(), seasonID, "4");
 		data.setValue(playerID, "TEST", "150");
-		data = new ScreeningData(getContext(), seasonID,"5");
+		data = new ScreeningData(getContext(), seasonID, "5");
+		assertEquals(expected, data.getAverageValue(playerID, "TEST"));
+		String differentSeason = "11";
+		ScreeningData data2 = new ScreeningData(getContext(), differentSeason , "6");
+		data2.setValue(playerID, "TEST", "1150");
 		assertEquals(expected, data.getAverageValue(playerID, "TEST"));
 	}
+
 	public void testPreviousResult() {
 		String expected = "200";
-		ScreeningData data = new ScreeningData(getContext(), seasonID,"1");
+		ScreeningData data = new ScreeningData(getContext(), seasonID, "1");
 		data.setValue(playerID, "TEST", expected);
-		data = new ScreeningData(getContext(), seasonID,"2");
+		data = new ScreeningData(getContext(), seasonID, "2");
 		assertEquals(expected, data.getPreviousValue(playerID, "TEST"));
 		expected = "300";
 		data.setValue(playerID, "TEST", expected);
-		data = new ScreeningData(getContext(), seasonID,"3");
+		data = new ScreeningData(getContext(), seasonID, "3");
 		assertEquals(expected, data.getPreviousValue(playerID, "TEST"));
 		expected = "350";
 		data.setValue(playerID, "TEST", expected);
-		data = new ScreeningData(getContext(), seasonID,"4");
+		data = new ScreeningData(getContext(), seasonID, "4");
 		assertEquals(expected, data.getPreviousValue(playerID, "TEST"));
 		expected = "150";
 		data.setValue(playerID, "TEST", expected);
-		data = new ScreeningData(getContext(), seasonID,"5");
+		data = new ScreeningData(getContext(), seasonID, "5");
 		assertEquals(expected, data.getPreviousValue(playerID, "TEST"));
 	}
+
 	public void testGetPlayerSeasonData() {
+		String differentSeason = "11";
 		ArrayList<Double> expected = new ArrayList<Double>();
-		ScreeningData data = new ScreeningData(getContext(), seasonID,"1");
+		ScreeningData data = new ScreeningData(getContext(), seasonID, "1");
 		data.setValue(playerID, "TEST", "200");
 		expected.add(200.00);
-		data = new ScreeningData(getContext(), seasonID,"2");
+		data = new ScreeningData(getContext(), seasonID, "2");
 		data.setValue(playerID, "TEST", "300");
 		expected.add(300.00);
-		data = new ScreeningData(getContext(), seasonID,"3");
+		data = new ScreeningData(getContext(), seasonID, "3");
 		data.setValue(playerID, "TEST", "350");
 		expected.add(350.00);
-		data = new ScreeningData(getContext(), seasonID,"4");
+		data = new ScreeningData(getContext(), seasonID, "4");
 		data.setValue(playerID, "TEST", "150");
 		expected.add(150.00);
+		data = new ScreeningData(getContext(), differentSeason, "5");
+		data.setValue(playerID, "TEST", "1150");
 		data = new ScreeningData(getContext(), seasonID);
-		assertEquals(expected,data.getPlayerSeasonData(playerID, "TEST"));
+		assertEquals(expected, data.getPlayerSeasonData(playerID, "TEST"));
 	}
 }
